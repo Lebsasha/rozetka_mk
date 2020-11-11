8c64a5e5246659a4853bc4370a8477ead11edeef
Alexander <lebedevskiy_s@rambler.ru>
2020-11-11 16:24:52 +0300

    Updated test

--- Tests/test.c
+++ Tests/test.c
@@ -2,11 +2,11 @@
 
 main()
 {
-    system("echo \"1\" > /dev/ttyACM0");
-    system("sleep 0.5");
-    system("echo \"0\" > /dev/ttyACM0");
-    system("sleep 0.5");
-    for (int i=1; i <1000; ++i)
+//    system("echo \"1\" > /dev/ttyACM0");
+//    system("sleep 0.5");
+//    system("echo \"0\" > /dev/ttyACM0");
+//    system("sleep 0.5");
+    for (int i=1; i <200; ++i)
     {
         system("echo \"0\" > /dev/ttyACM0");
         system("sleep 0.0001");
