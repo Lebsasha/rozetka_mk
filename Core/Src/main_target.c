147b6469b0010b92eeb78bdab2d2c8bf592a14fd
Alexander <lebedevskiy_s@rambler.ru>
2020-11-11 19:47:10 +0300

    First good result with 1 led

--- Core/Src/main_target.c
+++ Core/Src/main_target.c
@@ -11,9 +11,12 @@ extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim3;
 extern struct LED led[3];
 
-int str_cmp(uint8_t*, char*, size_t);
+int str_cmp(uint8_t*, char*,size_t);
+void toggle_led(const uint8_t* Buf, size_t i);
+
 void always_glow(struct LED*);
 void always_zero(struct LED*);
+
 void calc_up(struct LED* led);
 
 void calc_middle(struct LED* led);
@@ -25,20 +28,10 @@ void process_cmd(uint8_t* Buf, uint32_t* Len)
     char* cmd=(char*)Buf;
     if(*Len)
     {
-        if(strcmp(cmd, "first")>=0)
+        if(str_cmp(Buf, "first", sizeof("fisrt")-1)==0)//TODO Fix triggering
         {
             HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-            if(strcmp(cmd+sizeof("first "), "on") == 0)
-            {
-                HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-                led->curr_step=always_glow;
-            }
-            if(strcmp(cmd+sizeof("first "), "off") == 0)
-            {
-                led->curr_step=always_zero;
-            }
-            if(strcmp(cmd+sizeof("first "), "resume")==0)
-                led->curr_step=calc_up;
+            toggle_led(Buf, 0);
         }
 //        if(strcmp(cmd, "second") == 0)
 //        {
@@ -68,6 +61,25 @@ void process_cmd(uint8_t* Buf, uint32_t* Len)
             HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
         if(cmd[0]=='1')
             HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    }
+}
+
+void toggle_led(const uint8_t* Buf, size_t i)
+{
+    if(str_cmp(Buf+sizeof("first ")-1, "on",sizeof("on")-1) == 0)
+    {
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        led->curr_step=always_glow;
+    }
+    if(str_cmp(Buf+sizeof("first ")-1, "off",sizeof("off")-1) == 0)
+    {
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        led->curr_step=always_zero;
+    }
+    if(str_cmp(Buf+sizeof("first ")-1, "resume",sizeof("resume")-1) == 0)
+    {
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        led->curr_step=calc_up;
     }
 }
 
@@ -127,10 +139,14 @@ void my_delay(int mc_s)
     while (__HAL_TIM_GET_COUNTER(&htim3) < mc_s)
     {}
 }
-int str_cmp(uint8_t* buf, char* str, size_t n)
+int str_cmp(uint8_t* buf, char* str,size_t n)
 {
+//    size_t n=sizeof(str)-1;
     ++n;
     while (--n)
     {
+        if(*buf++!=*str++)
+            return 1;
     }
+    return 0;
 }
