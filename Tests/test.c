67ff2f8451cebe41807d4e6ef768137c0af1040e
Alexander <lebedevskiy_s@rambler.ru>
2020-11-11 16:20:45 +0300

    Added test

---
+++ Tests/test.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+main()
+{
+    system("echo \"1\" > /dev/ttyACM0");
+    system("sleep 0.5");
+    system("echo \"0\" > /dev/ttyACM0");
+    system("sleep 0.5");
+    for (int i=1; i <1000; ++i)
+    {
+        system("echo \"0\" > /dev/ttyACM0");
+        system("sleep 0.0001");
+    }
+}
