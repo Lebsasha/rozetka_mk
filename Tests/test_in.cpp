087057d2c9183b9efee7145599f7ee983f2d2bd2
Alexander <lebedevskiy_s@rambler.ru>
2020-11-20 22:26:45 +0300

    Try debug from CB

--- Tests/test_in.cpp
+++ Tests/test_in.cpp
@@ -1,13 +1,18 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-int main()
+int main(int argc, char** argv)
 {
 #define usb_ins 1
     std::ifstream dev("/dev/ttyACM0");
     std::ofstream send_cmd("/dev/ttyACM0");
-     int packets=1000;
-     int size_of_packet=8;
+    int packets=1000;
+    int size_of_packet=8;
+    if(argc==3)
+    {
+        packets=atoi(argv[1]);
+        size_of_packet=atoi(argv[2]);
+    }
     std::string cmd="211 "+std::to_string(packets)+' '+std::to_string(size_of_packet)+' ';
     if(!dev)
     {
@@ -22,11 +27,10 @@ int main()
     char s[packets*size_of_packet+100UL];
     send_cmd.write(cmd.c_str(), cmd.length());
     send_cmd.flush();
-    //if(usb_ins)
-    //for (int i=0; i <100000; ++i)
-    //{
-        dev.read(s, 100*size_of_packet);// "0123456789012345678901234567890123456789012345678901234567890123";
-        //dev.flush();
-    //}
-    std::cout<<std::string(s, 1000*size_of_packet)<<std::endl;
+    dev.read(s, packets*size_of_packet+sizeof("\n end")+sizeof(int));
+    std::cout<<(cmd=std::string(s, packets*size_of_packet+5))<<std::endl;
+    int time = strtol(cmd.c_str()+packets*size_of_packet+sizeof("\n end"), nullptr, 10);
+    std::cout<<time<<std::endl;
+    std::ofstream log("res.csv", std::ios_base::out|std::ios_base::app);
+    log<<size_of_packet<<", "<<time/packets<<", "<<time<<", "<<packets;
 }
