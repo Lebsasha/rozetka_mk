f776e99f0e7338732d55683cfdc711bb92f24ae4
Alexander <lebedevskiy_s@rambler.ru>
2020-11-23 00:10:42 +0300

    Some commit

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-#include <filesystem>
+//#include <filesystem>
 #include <iostream>
 
 int main()
@@ -12,35 +12,37 @@ int main()
 //    system("sleep 0.5");
 
 #define usb_ins 1
-    std::filesystem::path dev("/dev/ttyACM0");
-    if(std::filesystem::is_character_file(dev))
+    //std::filesystem::path dev("/dev/ttyACM0");
+    //if(std::filesystem::is_character_file(dev))
     if(usb_ins)
     {
         //./measure.out
         std::cout<<"Begin\n";
-        system("echo 1000000 8");
+        system("./measure.out 1000000 8");
+        std::cout<<"16 will beg\n";
+        system("sleep 10");
         std::cout<<"16 beg\n";
-        system("echo 1000000 16");
+        system("./measure.out 1000000 16");
+        std::cout<<"32 will beg\n";
+        system("sleep 10");
         std::cout<<"32 beg"<<std::endl;
-        system("echo 1000000 32");
+        system("./measure.out 1000000 32");
+        std::cout<<"64 will beg\n";
+        system("sleep 10");
         std::cout<<"64 beg"<<std::endl;
-        system("echo 1000000 64");
+        system("./measure.out 1000000 64");
+        std::cout<<"128 will beg\n";
+        system("sleep 10");
         std::cout<<"128 beg"<<std::endl;
-        system("echo 1000000 128");
+        system("./measure.out 1000000 128");
+        std::cout<<"256 will beg\n";
+        system("sleep 10");
         std::cout<<"256 beg"<<std::endl;
-        system("echo 1000000 256");
+        system("./measure.out 1000000 256");
+        std::cout<<"512 will beg\n";
+        system("sleep 10");
         std::cout<<"512 beg"<<std::endl;
-        system("echo 100000 512");
+        system("./measure.out 100000 512");
     }
     std::cout<<"End"<<std::endl;
-}
-str_cmp(u_int8_t * buf, char* str, size_t n)
-{
-    ++n;
-    while (--n)
-    {
-        if(*buf++!=*str++)
-            return 1;
-    }
-    return 0;fdfdfdfdf
 }
