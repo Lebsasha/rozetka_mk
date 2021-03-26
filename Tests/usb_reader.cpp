361e1cf344da17417c2770eea250018172b5d987
Alexander <lebedevskiys@yandex.by>
2021-03-26 20:07:08 +0300

    Bin. prot. on PC part is tested

--- Tests/usb_reader.cpp
+++ Tests/usb_reader.cpp
@@ -7,45 +7,42 @@
 
 using namespace std;
 
-enum {CC=0, LenL=1, LenH=2};
-class CommandBuilder
+///@brief this enum points on appropriate indexes in bin. prot.
+///e. g. buffer[CC], ...
+enum
+{
+    CC = 0, LenL = 1, LenH = 2
+};
+
+static const uint8_t SS_OFFSET = 42;
+
+class CommandWriter
 {
     static const size_t BUF_SIZE = 1024;
     char buffer[BUF_SIZE] = {0};
     size_t length;
 public:
-    CommandBuilder() : length(LenH+1)
+    CommandWriter() : length(LenH + 1)
     {
-        buffer[CC]=0x10;
-    }
-
-    void append(const string& s)
-    {
-        return append(s.c_str(), s.length());
-    }
-
-   void append(const char* s, size_t length_s)
-    {
-        assert(length + strlen(s) < BUF_SIZE);
-            strcpy(buffer + length, s);
-            length += strlen(s);
+        buffer[CC] = 0x10;///TODO enum
     }
 
     template<typename T>
     void append_var(T var)
     {
-        assert(length+sizeof(var)<BUF_SIZE);
+        assert(length + sizeof(var) < BUF_SIZE);
         *reinterpret_cast<T*>(buffer + length) = var;
         length += sizeof(var);
-        buffer[LenL]+=sizeof(var);///TODO!
+        buffer[LenL] += sizeof(var);///TODO!
     }
 
-    void write(ostream dev)
+    void write(ostream& dev)
     {
-        uint8_t sum=0;
-        for_each(buffer+CC+1, buffer+length, [&sum](char c){sum+=c;});
-        buffer[length]=sum;
-        length+=sizeof(sum);
+        uint8_t sum = SS_OFFSET;
+        for_each(buffer + CC + 1, buffer + length, [&sum](char c)
+        { sum += c; });
+        buffer[length] = sum;
+        length += sizeof(sum);
         dev.write(buffer, length);
         dev.flush();
     }
@@ -55,29 +52,45 @@ class CommandReader
 {
     static const size_t BUF_SIZE = 1024;
     char buffer[BUF_SIZE] = {0};
-    size_t length{};
+    size_t length;
+    size_t read_lehgth;
 public:
-    CommandReader() : length(0)
+    CommandReader() : length(0), read_lehgth(0)
     {}
 
-    char* read(istream dev)
+    char* read(istream& dev)
     {
-        dev.read(buffer, length = LenH+1);/// CC LenL LenH
-        const uint16_t n = (*reinterpret_cast<uint16_t*>(buffer + LenH) << 8) + *reinterpret_cast<uint16_t*>(buffer + LenL);
-        assert(n<BUF_SIZE);
+        dev.read(buffer, length = LenH + 1);/// CC LenL LenH
+        const uint16_t n = (*reinterpret_cast<uint8_t*>(buffer + LenH) << 8) + *reinterpret_cast<uint8_t*>(buffer + LenL);
+        assert(n < BUF_SIZE);
         dev.read(buffer + length, n);/// DD DD DD ... DD
         length += n;
-        dev.read(buffer + length, 1); /// SS
-        length+=1;
-        uint8_t sum = 0;
+        dev.read(buffer + length, sizeof (uint8_t)); /// SS
+        length += sizeof (uint8_t);
+        uint8_t sum = SS_OFFSET;
         for_each(buffer + LenL, buffer + length - 1, [&sum](char c)
         { sum += c; });
-        if (sum + 42 != buffer[length - 1])
+        if (sum /*+ SS_OFFSET*/ != (uint8_t) buffer[length - 1])///(weak TODO) Why?
         {
-            cerr << "SS" << endl;
+            cerr << "SS is't correct" << endl;
             assert(false);
         }
+        read_lehgth=LenH+1;
         return buffer;
+    }
+
+    template<typename T>
+    T get_param(T& param)
+    {
+       assert(length!=0);
+       param = *reinterpret_cast<T*>(buffer+read_lehgth);
+       read_lehgth+=sizeof(T);
+        return param;
+    }
+
+    uint8_t get_command(uint8_t& cmd)
+    {
+        return cmd=buffer[CC];
     }
 };
 
@@ -89,12 +102,16 @@ int main (int argc, char** argv)
 {
     const char* path="react_time.csv";
     ofstream stat(path, ios_base::app|ios_base::out);
-    const char* command="measure ";
+    CommandWriter command;
+    command.append_var<uint8_t>(0);/// Port TODO ?
+    command.append_var<uint16_t>(500);///Freq
+    CommandReader reader;
     char comp_command[]="sleep  ";
-    char time_buffer[64]={'0'};
-    uint32_t speed;
-    ofstream dev("/dev/ttyACM0");
-    ifstream read_time("/dev/ttyACM0");
+    uint8_t port_rec;
+    uint16_t freq_rec;
+    uint8_t command_rec;
+    ofstream dev("temp.log");
+    ifstream read_time("temp.log");
     if(!dev || !read_time)
     {
         std::cout << "Error opening COM file" << std::endl;
@@ -104,24 +121,20 @@ int main (int argc, char** argv)
     cout<<"begin ";///TODO Сделать "прозвон"
     for(size_t i =0;i<7;++i)
     {
-        comp_command[sizeof(comp_command) - 1 - 1]= rand() % 6 + '0';
+        comp_command[sizeof(comp_command) - 1 - 1]= '0';//rand() % 6 + '0';
         system(comp_command);
-        dev.write(command, strlen(command));
-        dev.flush();
-        read_time.read(time_buffer, sizeof(uint32_t));
-        if(!read_time)
-    {
-        std::cout<<endl<<"Error reading COM file in "<<i<<" iteration"<<std::endl;
-        return 2;
-    }
-        speed = *reinterpret_cast<uint32_t*>(time_buffer);
-        std::cout<<i<<' '<<speed<<endl;
-        stat << speed << endl;
+        command.write(dev);
+       reader.read(read_time);
+       reader.get_command(command_rec);
+       reader.get_param(port_rec);
+       reader.get_param(freq_rec);
+        std::cout << i << ' ' << command_rec<<port_rec << freq_rec << endl;
+//        stat << speed << endl;
         if(if_exit)
             break;
     }
     cout<<"end"<<endl;
-    calc_mean(path);
+    //calc_mean(path);
 }
 
 int calc_mean(const string& path)
